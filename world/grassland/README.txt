9692cdbf49547bf6ed9af495661a4d9cf9af3d33
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2020-12-27 22:17:26 +0100

    another try for world

---
+++ world/grassland/README.txt
@@ -0,0 +1,1 @@
+Downloaded from https://opengameart.org/content/whispers-of-avalon-grassland-tileset
---
+++ world/vortex.tsx
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="vortex" tilewidth="32" tileheight="32" tilecount="72" columns="12">
+ <image source="vortex.png" width="384" height="192"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="36" duration="100"/>
+   <frame tileid="39" duration="100"/>
+   <frame tileid="42" duration="100"/>
+   <frame tileid="45" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="37" duration="100"/>
+   <frame tileid="40" duration="100"/>
+   <frame tileid="43" duration="100"/>
+   <frame tileid="46" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="38" duration="100"/>
+   <frame tileid="41" duration="100"/>
+   <frame tileid="44" duration="100"/>
+   <frame tileid="47" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="48" duration="100"/>
+   <frame tileid="51" duration="100"/>
+   <frame tileid="54" duration="100"/>
+   <frame tileid="57" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="13">
+  <animation>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="49" duration="100"/>
+   <frame tileid="52" duration="100"/>
+   <frame tileid="55" duration="100"/>
+   <frame tileid="58" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="14">
+  <animation>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="50" duration="100"/>
+   <frame tileid="53" duration="100"/>
+   <frame tileid="56" duration="100"/>
+   <frame tileid="59" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="24">
+  <animation>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="60" duration="100"/>
+   <frame tileid="63" duration="100"/>
+   <frame tileid="66" duration="100"/>
+   <frame tileid="69" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="25">
+  <animation>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="61" duration="100"/>
+   <frame tileid="64" duration="100"/>
+   <frame tileid="67" duration="100"/>
+   <frame tileid="70" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="26">
+  <animation>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="35" duration="100"/>
+   <frame tileid="62" duration="100"/>
+   <frame tileid="65" duration="100"/>
+   <frame tileid="68" duration="100"/>
+   <frame tileid="71" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
